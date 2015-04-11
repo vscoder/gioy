bd5c04b4c37bbf1f561decaf36f6fe060fdd1d3e
Alexey Koloskov <koloskov@flexline.ru>
2015-04-11 10:12:50 +0300

    gioy/settings.py настройка временной зоны и языка

--- gioy/settings.py
+++ gioy/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
