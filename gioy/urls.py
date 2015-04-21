c3369a459d15359460c503e04cfc6311bfea3b8d
Alexey Koloskov <vsyscoder@gmail.com>
2015-04-21 13:05:17 +0300

    gioy/urls.py исправлен некорректный url для авторизации

--- gioy/urls.py
+++ gioy/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # url(r'^$', 'gioy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include('django.contrib.auth.urls'),
     url(r'^ads/', include('ads.urls', namespace='ads')),
     url(r'^admin/', include(admin.site.urls)),
-include('django.contrib.auth.urls'),
 ]
