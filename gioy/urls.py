5405a68e905a251351559a3204781f88b207f146
Alexey Koloskov <vsyscoder@gmail.com>
2015-04-25 09:46:18 +0300

    Включена тема twitter bootstrap для интерфейса администратора

--- gioy/urls.py
+++ gioy/urls.py
@@ -2,11 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
     # url(r'^$', 'gioy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include('django.contrib.auth.urls'),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^ads/', include('ads.urls', namespace='ads')),
     url(r'^admin/', include(admin.site.urls)),
 ]
