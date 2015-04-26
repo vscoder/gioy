32890208acd43653f8688ee74f0cfcaf0fd6e64b
Alexey Koloskov <vsyscoder@gmail.com>
2015-04-26 14:09:32 +0300

    Доработан внешний вид с использованием bootstrap3. Добавлена домашняя страница

--- gioy/settings.py
+++ gioy/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ads',
+    'home',
     'django_extensions',
+    'bootstrap3',
     'mptt',
 )
 
--- gioy/urls.py
+++ gioy/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', 'gioy.views.home', name='home'),
+    #url(r'^$', 'gioy.views.home', name='home'),
 
+    url(r'^$', 'home.views.index', name='home'),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^ads/', include('ads.urls', namespace='ads')),
     url(r'^admin/', include(admin.site.urls)),
