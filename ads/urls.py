f7d9cf3d4bf783ab2f6e149bde54fb27fef8063b
Alexey Koloskov <vsyscoder@gmail.com>
2015-05-02 21:28:07 +0300

    Категории

--- ads/urls.py
+++ ads/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # ex: /ads/5/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex: /ads/category/
-    url(r'^category/$', views.category, name='categories'),
+    url(r'^category/$', views.categories, name='categories'),
     # ex: /ads/category/4/
     url(r'^category/(?P<category_id>[0-9]+)/$', views.category_detail, name='category_detail'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.views import generic
 
@@ -28,9 +29,11 @@ class DetailView(generic.DetailView):
     # template_name = 'ads/ad_detail.html'
 
 
-def category(request):
-    return HttpResponse("Hello, world. You're at the categories index.")
-
+def categories(request):
+    return render_to_response("ads/categories.html",
+                    {'nodes': Category.objects.all()},
+                    context_instance=RequestContext(request))
 
 def category_detail(request, category_id):
+    #context = {}
     return HttpResponse("You're looking at Category {}".format(category_id))
